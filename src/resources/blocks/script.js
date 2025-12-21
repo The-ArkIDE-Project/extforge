ee391dcec81fd6406fbfd150251cda537a216abe
arc360 <voidyt16@gmail.com>
2025-12-21 12:22:59 -0600

    Add new "log" block

--- src/resources/blocks/script.js
+++ src/resources/blocks/script.js
@@ -26,6 +26,7 @@ function register() {
         const code = `eval(${INPUT})`;
         return `${code}\n`;
     })
+
     registerBlock(`${categoryPrefix}evalv`, {
         message0: 'eval %1',
         args0: [
@@ -44,6 +45,38 @@ function register() {
         const INPUT = javascriptGenerator.valueToCode(block, 'INPUT');
         const code = `eval(${INPUT})`;
         return [code, 0];
+    })
+
+    registerBlock(`${categoryPrefix}logb`, {
+        message0: '%1 %2',
+        args0: [
+            {
+                "type": "field_dropdown",
+                "name": "LOG_TYPE",
+                "options": [
+                    ["log", "log"],
+                    ["warn", "warn"],
+                    ["error", "error"],
+                    ["info", "info"]
+                ]
+            },
+            {
+                "type": "field_input",
+                "name": "INPUT",
+                "check": "String",
+                "text": "\"hello\"",
+                "acceptsBlocks": true
+            },
+        ],
+        previousStatement: null,
+        nextStatement: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const LOG_TYPE = block.getFieldValue('LOG_TYPE');
+        const INPUT = javascriptGenerator.valueToCode(block, 'INPUT');
+        const code = `console.${LOG_TYPE}(${INPUT})`;
+        return `${code}\n`;
     })
 }
 
